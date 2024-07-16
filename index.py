51867b38cb2355dc713386c4029ec37eb3d6aae2
Chad Cote <chadcote58@gmail.com>
2024-07-16 10:49:22 -0400

    Added string method to each class to print out a sentence describing what they are

--- index.py
+++ index.py
@@ -1,4 +1,22 @@
-from slithering import cobra,python
-from swimming import crocodile,dolphin,frog,otter,penguin,shark,turtle,whale
-from walking import eagle,gecko,kangaroo,llama
+from slithering.cobra import Cobra
+from slithering.python import Python
+from swimming.crocodile import Crocodile
+from swimming.dolphin import Dolphin
+from swimming.frog import Frog
+from swimming.otter import Otter
+from swimming.penguin import Penguin
+from swimming.shark import Shark
+from swimming.turtle import Turtle
+from swimming.whale import Whale
+from walking.eagle import Eagle
+from walking.gecko import Gecko
+from walking.kangaroo import Kangaroo
+from walking.llama import Llama  # Import the class Llama, not the module
 
+
+miss_fuzz = Llama("Miss Fuzz", "domestic llama", "morning", "Llama Chow" )
+
+bob = Gecko("bob", "gecko", "midday", "Gecko Chow")
+print(bob)
+
+miss_fuzz.feed()
